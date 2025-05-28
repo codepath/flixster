fbe61ef074f6fde395ac9906e7d8c9f1953b0e30
kyra-ptn <kyra@codepath.org>
2025-05-28 16:06:01 -0700

    Add clear button to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,18 +9,21 @@ const SearchBar = ({ onSearch }) => {
     onSearch(query)
   }
 
+  const handleClear = () => {
+    setQuery('')
+    onSearch('') 
+  }
+
   return (
     <form className="search-bar" onSubmit={handleSubmit}>
-
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search for movies"
       />
-
       <button type="submit">Search</button>
-
+      <button type="button" onClick={handleClear}>Clear</button>
     </form>
   )
 }
